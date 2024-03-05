8d86316d42996996c68057bb5f56f41e4d8ffc96
YswMxl <yushan.weng_x902@escuelaces.net>
2024-03-05 16:50:57 +0100

    Escena duplicada

--- CookingWars/Assets/Scripts/Player/Player1.cs
+++ CookingWars/Assets/Scripts/Player/Player1.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Player1 : PlayerControl
 {
-    // Start is called before the first frame update
-    // Update is called once per frame
     void Update()
     {
         Vector3 playerInput = new Vector3
